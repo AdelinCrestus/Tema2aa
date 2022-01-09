12b5bfbc9ec8b21e07464a391eb62992b20a2ad6
AdelinCrestus <adelin_crestus@DESKTOP-NOIG1IS.localdomain>
2022-01-10 00:59:32 +0200

    Ready

---
+++ Variabila.java
@@ -0,0 +1,25 @@
+public class Variabila {
+    private Integer index;
+    private Integer registru;
+
+    public Variabila(int index, int registru) {
+        this.index = index;
+        this.registru = registru;
+    }
+
+    public Integer getIndex() {
+        return index;
+    }
+
+    public void setIndex(int index) {
+        this.index = index;
+    }
+
+    public Integer getRegistru() {
+        return registru;
+    }
+
+    public void setRegistru(int registru) {
+        this.registru = registru;
+    }
+}
